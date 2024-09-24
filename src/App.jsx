346100e04425a3dc7c5d52206c405399f0c68a35
Kateryna-Katya <katyaukr2014@gmail.com>
2024-09-24 13:09:33 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,18 @@
-import Profile from `./components/Profile/Profile.jsx`
-export default App
+import Profile from "./components/Profile/Profile.jsx";
+import userData from "../src/userData.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,2 +1,36 @@
 import clsx from "clsx";
 import style from "./Profile.module.css";
+
+function Profile({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={clsx(style.card)}>
+      <div className={clsx(style.info)}>
+        <img className={clsx(style.img)} src={image} alt="User avatar" />
+        <p className={clsx(style.name)}>{name}</p>
+        <p className={clsx(style.tag)}>@{tag}</p>
+        <p className={clsx(style.location)}>{location}</p>
+      </div>
+      <ul className={clsx(style.list)}>
+        <li className={clsx(style.item)}>
+          <span className={clsx(style.statName)}>Followers</span>
+          <span className={clsx(style.statValue)}>{followers}</span>
+        </li>
+        <li className={clsx(style.item)}>
+          <span className={clsx(style.statName)}>Views</span>
+          <span className={clsx(style.statValue)}>{views}</span>
+        </li>
+        <li className={clsx(style.item)}>
+          <span className={clsx(style.statName)}>Likes</span>
+          <span className={clsx(style.statValue)}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Profile;
